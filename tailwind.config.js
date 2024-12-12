686664d84ea5f80c4d21b3baeb2b72d29c314fa3
Kalil Fernandes <kalilfernandes@hotmail.com>
2024-12-12 13:35:38 -0300

    burguer

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./**/*.{html,js}"],
+  theme: {
+    
+    }
+  }
+    extend: {
+      backgroundImage:{
+        "home"; "url('/assets/bg.png')"
+      }
+    }
+
+  plugins: []
+
+
+
